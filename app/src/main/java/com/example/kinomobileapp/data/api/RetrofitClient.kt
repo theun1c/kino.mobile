5cbf26414b26ce2d539f4f32dd7891d4150e0ef9
theun1c <belovdima1122@yandex.ru>
2026-01-18 18:16:36 +0300

    add retrofit client

---
+++ app/src/main/java/com/example/kinomobileapp/data/api/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.example.kinomobileapp.data.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://127.0.0.1:8090/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val pocketBaseApi: PocketBaseApi by lazy {
+        retrofit.create(PocketBaseApi::class.java)
+    }
+}
