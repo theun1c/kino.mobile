650dc447e62430997021ef974f588a5dad57c4b1
theun1c <belovdima1122@yandex.ru>
2026-01-18 18:33:04 +0300

    update movie catalog screen

--- app/src/main/java/com/example/kinomobileapp/ui/screens/MovieCatalogScreen.kt
+++ app/src/main/java/com/example/kinomobileapp/ui/screens/MovieCatalogScreen.kt
@@ -3,6 +3,7 @@ package com.example.kinomobileapp.ui.screens
 import android.media.Rating
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,17 +14,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.kinomobileapp.ui.components.MovieCard
 import com.example.kinomobileapp.R
+import com.example.kinomobileapp.ui.viewmodels.MovieViewModel
 
 
 data class MovieData(
@@ -32,14 +39,12 @@ data class MovieData(
 )
 
 @Composable
-fun MovieCatalogScreen(){
-
-    val movieList = listOf(
-        MovieData("8.1", "Интерстеллар"),
-        MovieData("3.1", "Интерстеллар55"),
-        MovieData("4.1", "Интерстеллар3"),
-        MovieData("1.1", "Интерстеллар2"),
-    )
+fun MovieCatalogScreen(
+    viewModel: MovieViewModel = hiltViewModel()
+){
+    val movies by viewModel.movies.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val error by viewModel.error.collectAsState()
 
     Column(
         modifier = Modifier
@@ -52,45 +57,82 @@ fun MovieCatalogScreen(){
             color = Color(0xFF17418C)
         )
 
-        // lazy column with film-card components
+        Spacer(modifier = Modifier.height(16.dp))
 
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
+        if(isLoading){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else if (error != null){
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = error ?: "Error",
+                    color = Color.Red
+                )
 
-            items(movieList) { movie ->
-                Row (
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
+                Spacer(modifier = Modifier.height(16.dp))
 
+                Button(
+                    onClick = { viewModel.loadMovies() }
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.trash_img),
-
-                        contentDescription = "trash img",
-                        modifier = Modifier
-                            .width(20.dp)
-                            .height(20.dp)
-                    )
-
-                    Spacer(modifier = Modifier.width(10.dp))
-
-                    MovieCard(
-                        cardRating = movie.rating,
-                        cardName = movie.name
-                    )
+                    Text(text = "Retry")
                 }
-                // Разделитель после каждого элемента, кроме последнего
-                if (movie != movieList.last()) {
-                    Divider(
-                        modifier = Modifier.padding(vertical = 4.dp),
-                        color = Color(0xFF4CBBBF),
-                        thickness = 1.dp,
-                    )
+            }
+        } else if (movies.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Фильмы не найдены")
+            }
+        } else {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                items(movies) { movie ->
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.trash_img),
+
+                            contentDescription = "trash img",
+                            modifier = Modifier
+                                .width(20.dp)
+                                .height(20.dp)
+                        )
+
+                        Spacer(modifier = Modifier.width(10.dp))
+
+                        MovieCard(
+                            movie = movie,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                    // Разделитель после каждого элемента, кроме последнего
+                    if (movie != movies.last()) {
+                        Divider(
+                            modifier = Modifier.padding(vertical = 4.dp),
+                            color = Color(0xFF4CBBBF),
+                            thickness = 1.dp,
+                        )
+                    }
                 }
             }
         }
+
+
     }
 }
 
