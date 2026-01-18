b7716c14fd414a4fef61924691d5527177e99fed
theun1c <belovdima1122@yandex.ru>
2026-01-18 19:17:22 +0300

    update pb api for delete

--- app/src/main/java/com/example/kinomobileapp/data/api/PocketBaseApi.kt
+++ app/src/main/java/com/example/kinomobileapp/data/api/PocketBaseApi.kt
@@ -1,7 +1,10 @@
 package com.example.kinomobileapp.data.api
 
 import com.example.kinomobileapp.domain.models.MovieResponse
+import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PocketBaseApi {
@@ -10,4 +13,7 @@ interface PocketBaseApi {
         @Query("page") page: Int = 1,
         @Query("perPage") perPage: Int = 50
     ) : MovieResponse
+
+    @DELETE("/api/collections/movies/records/{id}")
+    suspend fun deleteMovie(@Path("id") id: String): Response<Unit>
 }
